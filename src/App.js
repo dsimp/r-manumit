09de2ebb58e3dfe1a6b9ae6ea8a5b9f56f61df59
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2025-08-10 17:35:34 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { useRef } from 'react';
-import * as THREE from 'three';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import * as THREE from "three";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Scene() {
   const mesh = useRef();
@@ -10,12 +12,23 @@ function Scene() {
   return (
     <mesh ref={mesh} rotation={[0, 0, 0]}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color='blue' />
+      <meshStandardMaterial color="blue" />
     </mesh>
   );
 }
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/api/hello`)
+      .then((res) => {
+        console.log("Backend says:", res.data);
+      })
+      .catch((err) => {
+        console.error("Error connecting to backend:", err);
+      });
+  }, []);
+
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
